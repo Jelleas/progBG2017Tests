31e280331611fc4fe0c977cf6a162c1aa045ad8b
jelleas <jelle.van.assema@gmail.com>
2016-12-26 15:13:07 +0100

    first commit

---
+++ tests/bg2017/module1/helloTest.py
@@ -0,0 +1,14 @@
+import checkpy.tests as t
+import checkpy.lib as lib
+import checkpy.assertlib as assertlib
+
+@t.test(0)
+def exactHello(test):
+	test.test = lambda : assertlib.exact(lib.outputOf(_fileName).split("\n")[0], "Hello, world!")
+	test.description = lambda : "print precies: Hello, world!"
+
+@t.failed(exactHello)
+@t.test(1)
+def oneLine(test):
+	test.test = lambda : assertlib.exact(len(lib.outputOf(_fileName).split("\n")), 2)
+	test.description = lambda : "print precies één regel"
