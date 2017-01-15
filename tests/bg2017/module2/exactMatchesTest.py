74bc40b605396a3354bfcf3fd854f4d24bc222c1
Maarten <maarten.inja@gmail.com>
2017-01-15 21:52:16 +0100

    part 1 done

--- tests/bg2017/module2/exactMatchesTest.py
+++ tests/bg2017/module2/exactMatchesTest.py
@@ -2,30 +2,7 @@ import checkpy.tests as t
 import checkpy.lib as lib
 import checkpy.assertlib as assertlib
 
-#@t.test(0)
-#def exactChange0(test):
-#	test.test = lambda : assertlib.numberOnLine(0, lib.getLine(lib.outputOf(_fileName, [0]), 0))
-#	test.description = lambda : "0$ aan wisselgeld staat gelijk aan 0 munten"
-#
-#@t.test(1)
-#def exactChange41(test):
-#	test.test = lambda : assertlib.numberOnLine(4, lib.getLine(lib.outputOf(_fileName, [0.41]), 0))
-#	test.description = lambda : "0.41$ aan wisselgeld staat gelijk aan 4 munten"
-#
-#@t.test(2)
-#def exactChange9999(test):
-#	test.test = lambda : assertlib.numberOnLine(39996, lib.getLine(lib.outputOf(_fileName, [9999]), 0))
-#	test.description = lambda : "9999$ aan wisselgeld staat gelijk aan 39996 munten"
-#
-#@t.test(3)
-#def exactChange402(test):
-#	test.test = lambda : assertlib.numberOnLine(18, lib.getLine(lib.outputOf(_fileName, [4.02]), 0))
-#	test.description = lambda : "4.02$ aan wisselgeld staat gelijk aan 18 munten"
-#
-#@t.test(10)
-#def handlesWrongInput(test):
-#	test.test = lambda : assertlib.numberOnLine(0, lib.getLine(lib.outputOf(_fileName, [-9.50, -327, 0]), 0))
-#	test.description = lambda : "handelt een verkeerde input van -9.50 en -327 af"
+#### count_exact_matches
 
 @t.test(0)
 def has_count_exact_matches(test):
@@ -40,7 +17,7 @@ def is_int_count_exact_matches(test):
 
 @t.passed(has_count_exact_matches)
 @t.test(2)
-def correct_count_exact_matches(test):
+def count_exact_matches_2(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("atgacatgcacaagtatgcat", "atgc"), 2)
@@ -48,15 +25,15 @@ def correct_count_exact_matches(test):
 
 @t.passed(has_count_exact_matches)
 @t.test(3)
-def correct_count_exact_matches(test):
+def count_exact_matches_8(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
-        _fileName)("atgacatgcacaagtatgcat", "a"),  4)
+        _fileName)("atgacatgcacaagtatgcat", "a"),  8)
     test.description = lambda : "count_exact_matches werkt voor invoer 'atgacatgcacaagtatgcat', 'a'"
 
 @t.passed(has_count_exact_matches)
 @t.test(4)
-def correct_count_exact_matches(test):
+def count_exact_matches_0a(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("atgacatgcacaagtatgcat", "b"), 0)
@@ -64,8 +41,75 @@ def correct_count_exact_matches(test):
 
 @t.passed(has_count_exact_matches)
 @t.test(5)
-def correct_count_exact_matches(test):
+def count_exact_matches_0b(test):
     test.test = lambda : assertlib.exact(
         lib.getFunction("count_exact_matches",
         _fileName)("", "atgc"), 0)
     test.description = lambda : "count_exact_matches werkt voor invoer '', 'atgc'"
+
+
+
+#### exact_matches
+
+@t.test(6)
+def has_exact_matches(test):
+    test.test = lambda : assertlib.fileContainsFunctionDefinitions(_fileName, "count_exact_matches")
+    test.description = lambda : "definieert de functie count_exact_matches()"
+
+@t.passed(has_exact_matches)
+@t.test(7)
+def is_list_exact_matches(test):
+    test.test = lambda : assertlib.sameType(lib.getFunction("exact_matches", _fileName)("a", "a"), [])
+    test.description = lambda : "exact_matches geeft een lijst terug"
+
+@t.passed(has_exact_matches)
+@t.test(8)
+def exact_matches_a(test):
+    test.test = lambda : set(lib.getFunction("exact_matches",
+        _fileName)("atgacatgcacaagtatgcat", "atgc")) == set([5,15])
+    test.description = lambda : "exact_matches werkt voor invoer 'atgacatgcacaagtatgcat', 'atgc'"
+
+@t.passed(has_exact_matches)
+@t.test(9)
+def exact_matches_b(test):
+    test.test = lambda : set(lib.getFunction("exact_matches",
+        _fileName)("atgacatgca", "a")) == set([0, 3, 5, 9])
+    test.description = lambda : "exact_matches werkt voor invoer 'atgacatgca', 'a'"
+
+@t.passed(has_exact_matches)
+@t.test(10)
+def exact_matches_c(test):
+    test.test = lambda : set(lib.getFunction("exact_matches",
+        _fileName)("atgacatgca", "b")) == set([])
+    test.description = lambda : "exact_matches werkt voor invoer 'atgacatgca', 'b'"
+
+@t.passed(has_exact_matches)
+@t.test(11)
+def exact_matches_c(test):
+    test.test = lambda : set(lib.getFunction("exact_matches",
+        _fileName)("atgacatgca", "")) == set(range(10))
+    test.description = lambda : "exact_matches werkt voor invoer 'atgacatgca', ''"
+
+@t.passed(has_exact_matches)
+@t.test(12)
+def exact_matches_c(test):
+    test.test = lambda : set(lib.getFunction("exact_matches",
+        _fileName)("", "test")) == set()
+    test.description = lambda : "exact_matches werkt voor invoer '', 'test'"
+
+
+
+#print exact_matches("atgacatgcacaagtatgcat", "atgc")
+#print exact_matches("atgacatgca", "a")
+#print exact_matches("atgacatgca", "b")
+#print exact_matches("atgacatgca", "")
+#print exact_matches("", "test")
+#[5, 15]
+#[0, 3, 5, 9]
+#[]
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#[]
+
+
+
+
