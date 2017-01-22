e09e09fa6c5981eeb5044e05dee0907a79dc2a56
jelleas <jelle.van.assema@gmail.com>
2017-01-22 14:15:50 +0100

    m -> M

--- tests/bg2017/module3/angry_birdsTest.py
+++ tests/bg2017/module3/angry_birdsTest.py
@@ -24,7 +24,7 @@ def correctOutput(test):
 					break
 			else:
 				nCorrect -= 1
-		return nCorrect == nAnswers, "miste o.a. verwachte antwoorden {}\nVond o.a. op de volgende regels een niet kloppend antwoord: {}".format(answers[:2], lines[:2])
+		return nCorrect == nAnswers, "Miste o.a. verwachte antwoorden {}\nVond o.a. op de volgende regels een niet kloppend antwoord: {}".format(answers[:2], lines[:2])
 
 	test.test = testMethod
 	test.description = lambda : "print de juiste hoeken waarbij de vogel de sensor raakt"
