2bb66179509e9e0cc56e763f8d94096348077524
jelleas <jelle.van.assema@gmail.com>
2017-01-29 19:19:03 +0100

    module4 tests

---
+++ tests/bg2017/module4/temperatuurTest.py
@@ -0,0 +1,47 @@
+import checkpy.tests as t
+import checkpy.lib as lib
+import checkpy.assertlib as assertlib
+
+@t.test(0)
+def correctMaxTemp(test):
+	test.test = lambda : assertlib.match(lib.getLine(lib.outputOf(_fileName), 0), ".*36[,.]8.*")
+	test.description = lambda : "print de maximale temperatuur"
+
+@t.test(01)
+def correctDayOfMaxTemp(test):
+	def testMethod():
+		line = lib.getLine(lib.outputOf(_fileName), 0)
+		return assertlib.contains(line, "27") and assertlib.contains(line.lower(), "jun") and assertlib.contains(line, "1947")
+	test.test = testMethod
+	test.description = lambda : "print de dag van de maximale temperatuur"
+
+@t.test(10)
+def correctMinTemp(test):
+	test.test = lambda : assertlib.match(lib.getLine(lib.outputOf(_fileName), 1), ".*24[,.]8.*")
+	test.description = lambda : "print de minimale temperatuur"
+
+@t.test(11)
+def correctDayOfMinTemp(test):
+	def testMethod():
+		line = lib.getLine(lib.outputOf(_fileName), 1)
+		return assertlib.contains(line, "27") and assertlib.contains(line.lower(), "jan") and assertlib.contains(line, "1942")
+	test.test = testMethod
+	test.description = lambda : "print de dag van de minimale temperatuur"
+
+@t.test(20)
+def correctLongestPeriod(test):
+	test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 2), "21")
+	test.description = lambda : "print de langste periode dat het aaneengesloten heeft gevroren"
+
+@t.test(21)
+def correctFinalDayOfLongestPeriod(test):
+	def testMethod():
+		line = lib.getLine(lib.outputOf(_fileName), 2)
+		return assertlib.contains(line, "24") and assertlib.contains(line.lower(), "feb") and assertlib.contains(line, "1947")
+	test.test = testMethod
+	test.description = lambda : "print de laatste dag van de langste periode dat het aaneengesloten heeft gevroren"
+
+@t.test(30)
+def correctHeatwave(test):
+	test.test = lambda : assertlib.contains(lib.getLine(lib.outputOf(_fileName), 3), "1911")
+	test.description = lambda : "print het eerste jaartal waarin een hittegolf voorkwam"
